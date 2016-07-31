d2b8079084b95bb289170c8ffefddda4211326c6
RamyAllam <linux@ramyallam.com>
2016-07-31 07:49:24 +0200

    Feature: Add button to enable and disable server from server info page

--- rsynchero_web/serversmanage/urls.py
+++ rsynchero_web/serversmanage/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     url(r'^server/(?P<pk>[0-9]+)/edit/$', views.ServerUpdate.as_view(), name='serveredit'),
     url(r'^server/(?P<pk>[0-9]+)/delete/$', views.ServerDelete.as_view(), name='serverdelete'),
     url(r'^runbackup/$', views.request_page_cmd, name='runcommand'),
+    url(r'^server/(?P<server_id>[0-9]+)/disable/$', views.serverdisable, name='serverdisable'),
+    url(r'^server/(?P<server_id>[0-9]+)/enable/$', views.serverenable, name='serverenable'),
     url(r'^login/$', login),
     url(r'^logout/$', logout, {'next_page': '/'}),
 ]
--- rsynchero_web/serversmanage/views.py
+++ rsynchero_web/serversmanage/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from . models import servers
+from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
 from django.http import HttpResponseRedirect
 
@@ -42,4 +43,18 @@ def request_page_cmd(request):
         server_ip = request.GET.get('serverip')
         main_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "../../")
         os.system("cd %s ; python3 main.py --ip=%s &" % (main_dir, server_ip))
+    return HttpResponseRedirect('/server/' + server_id)
+
+
+def serverdisable(request, server_id):
+    server = get_object_or_404(servers, pk=server_id)
+    server.serverstatus = 'Disabled'
+    server.save()
+    return HttpResponseRedirect('/server/' + server_id)
+
+
+def serverenable(request, server_id):
+    server = get_object_or_404(servers, pk=server_id)
+    server.serverstatus = 'Enabled'
+    server.save()
     return HttpResponseRedirect('/server/' + server_id)
